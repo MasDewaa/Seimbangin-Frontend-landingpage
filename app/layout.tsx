d2e97017f8f286375ee94ca4101d9001a84f4084
masdewaa <ramasyailana3@gmail.com>
2025-07-01 16:45:15 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0FSJJZV93J"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0FSJJZV93J');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
